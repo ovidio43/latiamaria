18c1b72862b788012f73f4aff36171b8a8b07747
Aldo Adachi <aldo.adachi@iconicweb.com>
2014-11-14 16:41:43 -0400

    edd

--- wp-content/themes/themeLatiamaria/template-homepage.php
+++ wp-content/themes/themeLatiamaria/template-homepage.php
@@ -5,7 +5,7 @@
 get_header();
 ?>
     <div class="container">
-      <div class="wrap-video" style="background-color: #fff;">
+      <div class="wrap-video" style="background-color: #fff; text-align:center;">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
               <?php the_content();?>
